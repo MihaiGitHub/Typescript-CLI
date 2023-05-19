38c6546305d8ce4f87da1316ff0738eaa9cc2b8c
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-05-19 16:30:13 -0700

    Use unpkg plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   // assign a type to this ref
@@ -27,12 +28,14 @@ const App = () => {
     }
 
     // this will only do some transpiling on input
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
 
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   return (

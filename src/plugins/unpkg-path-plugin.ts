838719e82a9828a89d68183a7e20d3111c5c5433
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-05-22 13:44:37 -0700

    Update plugin to dynamically fetch modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,4 +1,5 @@
 import * as esbuild from "esbuild-wasm";
+import axios from 'axios';
 
 export const unpkgPathPlugin = () => {
   return {
@@ -20,15 +21,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const message = require('tiny-test-pkg)';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
+        } 
+
+        const { data } = await axios.get(args.path);
+
+        return {
+          loader: 'jsx',
+          contents: data
         }
       });
     },

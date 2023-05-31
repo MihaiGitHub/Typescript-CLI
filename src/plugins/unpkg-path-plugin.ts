81ff49f23f613d418aa5b92781695d1eb24f0ccf
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-05-31 13:43:30 -0700

    Update to use ES Modules syntax

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -34,7 +34,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('nested-test-pkg)';
+              import React, { useState } from React;
               console.log(message);
             `,
           };

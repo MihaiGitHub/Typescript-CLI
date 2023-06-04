daf7df22d5d6a1ccb881f4a3ae18912eef1fe2cb
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-04 09:42:31 -0700

    Fix Typescript errors

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -53,7 +53,9 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        const cachedResult = await fileCache.getItem(args.path);
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
 
         if (cachedResult) {
           return cachedResult;
@@ -61,7 +63,7 @@ export const unpkgPathPlugin = () => {
 
         const { data, request } = await axios.get(args.path);
 
-        const result = {
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,

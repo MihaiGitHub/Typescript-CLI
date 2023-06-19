814337ca28479728650c008c3576109a3601b01c
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-19 10:14:42 -0700

    Refactor and extract bundle code

--- src/index.tsx
+++ src/index.tsx
@@ -2,48 +2,17 @@ import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
-import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
+import bundle from "./bundler";
 
 const App = () => {
-  // assign a type to this ref
-  const ref = useRef<any>();
   const [code, setCode] = useState("");
   const [input, setInput] = useState("");
 
-  const startService = async () => {
-    // assign the esbuild service to the ref
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, []);
-
   const onClick = async () => {
-    // if user clicks on submit button before service is ready
-    if (!ref.current) {
-      return;
-    }
-
-    // this will only do some transpiling on input
-    const result = await ref.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
 
     // try {
     //   // execute javascript that is inside of a string

c3fbc5ffa0271d35e60f4edcd2622516e4280415
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-20 09:26:27 -0700

    Code refactor

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundle from "../bundler";
+
+const CodeCell = () => {
+  const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
+
+  const onClick = async () => {
+    const output = await bundle(input);
+    setCode(output);
+
+    // try {
+    //   // execute javascript that is inside of a string
+    //   eval(result.outputFiles[0].text);
+    // } catch (err) {
+    //   alert(err);
+    // }
+  };
+
+  return (
+    <div>
+      <CodeEditor
+        initialValue="const a = 1;"
+        onChange={(value) => setInput(value)}
+      />
+
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      {/* sandbox property allows or disallows direct access between iframe and parent */}
+      {/* sandbox="allow-scripts" allows iframe to execute script tags */}
+      {/* <iframe title="Preview" sandbox="allow-same-origin" src="/iframe.html" /> */}
+
+      {/* load up content into this iframe using a local string */}
+      {/* <iframe
+        title="Preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      /> */}
+      <Preview code={code} />
+    </div>
+  );
+};
+
+export default CodeCell;
--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,11 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useState } from "react";
 import ReactDOM from "react-dom";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
+import CodeCell from "./components/code-cell";
 
 const App = () => {
-  const [code, setCode] = useState("");
-  const [input, setInput] = useState("");
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-
-    // try {
-    //   // execute javascript that is inside of a string
-    //   eval(result.outputFiles[0].text);
-    // } catch (err) {
-    //   alert(err);
-    // }
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue="const a = 1;"
-        onChange={(value) => setInput(value)}
-      />
-
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      {/* sandbox property allows or disallows direct access between iframe and parent */}
-      {/* sandbox="allow-scripts" allows iframe to execute script tags */}
-      {/* <iframe title="Preview" sandbox="allow-same-origin" src="/iframe.html" /> */}
-
-      {/* load up content into this iframe using a local string */}
-      {/* <iframe
-        title="Preview"
-        ref={iframe}
-        sandbox="allow-scripts"
-        srcDoc={html}
-      /> */}
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };

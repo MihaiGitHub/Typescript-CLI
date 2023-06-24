f9aa94faa21b974ab47d2d853d0836a611003950
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-06-24 15:32:29 -0700

    Create prop definitions

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,15 +1,22 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
 interface ResizableProps {
   direction: "horizontal" | "vertical";
 }
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {};
+  } else {
+    resizableProps = {};
+  }
   return (
     <ResizableBox
       minConstraints={[Infinity, 24]}
       maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      height={300}
+      height={direction === "vertical" ? 300 : Infinity}
       width={Infinity}
       resizeHandles={["s"]}
     >

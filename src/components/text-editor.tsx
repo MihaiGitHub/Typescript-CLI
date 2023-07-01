2f8b22edaffa3a33ff5fb3ff0a972562536761aa
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-01 12:53:34 -0700

    Restyle the resize bar

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -27,13 +27,13 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
+      <div ref={ref} className="text-editor">
         <MDEditor />
       </div>
     );
   }
   return (
-    <div onClick={() => setEditing(true)}>
+    <div className="text-editor" onClick={() => setEditing(true)}>
       <MDEditor.Markdown source={"# Header"} />
     </div>
   );

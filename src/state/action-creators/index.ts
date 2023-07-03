bd9fd512c29cc0b30cd217b49b5001159b493901
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-03 10:58:33 -0700

    Action creator implementation

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -6,10 +6,48 @@ import {
   MoveCellAction,
   InsertCellBeforeAction,
 } from "../actions";
+import { CellTypes } from "../cell";
 
-export const updateCell = (): UpdateCellAction => {
+// takes in id and content arguments of type string and returns an object of type UpdateCellAction
+export const updateCell = (id: string, content: string): UpdateCellAction => {
   // must return object that satisfies interface UpdateCellAction
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
 };
-export const deleteCell = (): DeleteCellAction => {};
-export const moveCell = (): MoveCellAction => {};
-export const insertCellBefore = (): InsertCellBeforeAction => {};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (
+  id: string,
+  direction: "up" | "down"
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+export const insertCellBefore = (
+  id: string,
+  cellType: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType,
+    },
+  };
+};

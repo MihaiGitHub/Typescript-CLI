340ab2f5228d73c4c0e92834a60db3504c975f6b
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-04 09:13:30 -0700

    Manual testing of redux store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
+import { ActionType } from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+// Manual testing of Redux store
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: "code",
+  },
+});
+
+console.log(store.getState());
+//////////////////////////////////////

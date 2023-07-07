348a7557bf41a2db8f54c5bacf4e2f74d0052175
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-06 17:50:32 -0700

    Add cell component

---
+++ src/components/add-cell.tsx
@@ -0,0 +1,16 @@
+import "./add-cell.css";
+import { useActions } from "../hooks/use-actions";
+
+interface AddCellProps {
+  nextCellid: string;
+}
+
+const AddCell: React.FC<AddCellProps> = ({ nextCellid }) => {
+  const { insertCellBefore } = useActions();
+  return (
+    <div>
+      <button onClick={() => insertCellBefore(nextCellid, "code")}>Code</button>
+      <button onClick={() => insertCellBefore(nextCellid, "text")}>Code</button>
+    </div>
+  );
+};

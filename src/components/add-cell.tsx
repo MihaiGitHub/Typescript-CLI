468a7d84feabf68fd2cdc8b70def875b1078a152
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-07 17:01:14 -0700

    Update button styling

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -9,8 +9,26 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellid }) => {
   const { insertCellBefore } = useActions();
   return (
     <div className="add-cell">
-      <button onClick={() => insertCellBefore(nextCellid, "code")}>Code</button>
-      <button onClick={() => insertCellBefore(nextCellid, "text")}>Code</button>
+      <div className="add-buttons">
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellid, "code")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Code</span>
+        </button>
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellid, "text")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
       <div className="divider"></div>
     </div>
   );

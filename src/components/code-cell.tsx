3ea6539c3af3b65935a1512b89a55c8fa15549a3
Mihai Smarandache <mihai.sanfran@gmail.com>
2023-07-09 14:03:49 -0700

    Implement a show functions No-Op

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,14 +16,13 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
-
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [
-      `
+
+    const showFunc = `
     import _React from 'react';
     import _ReactDOM from 'react-dom';
 
-    const show = (value) => {
+    var show = (value) => {
       const root = document.querySelector('#root');
 
       if(typeof value === 'object'){
@@ -35,11 +34,20 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
       } else {
         root.innerHTML = value;
       }
-    }`,
-    ];
+    }`;
+
+    const showFuncNoop = "var show = () => {}";
+
+    const cumulativeCode = [];
 
     for (let c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop);
+        }
+
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
